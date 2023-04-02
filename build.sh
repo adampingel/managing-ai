d4ec8cbe597c76cc7c50f6897936ab229ba265f3
Adam Pingel <pingel@gmail.com>
2023-04-02 00:36:45 -0400

    echo site url

--- build.sh
+++ build.sh
@@ -12,3 +12,5 @@ git push
 ghp-import -n -p -f _build/html
 
 echo "Browse to https://github.com/adampingel/managing-ai/actions to check progress"
+
+echo "Browse to https://adampingel.github.io/managing-ai/ to see the updates live"
